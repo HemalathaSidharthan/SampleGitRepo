445304cb492cc2bb228c21bf252cdf840daf58fc
Admin <Admin@Admin-PC>
2022-03-24 17:42:34 +0530

    Tc_1

---
+++ commercial/src/test/java/samplePackage1/SampleTest.java
@@ -0,0 +1,13 @@
+package samplePackage1;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public void main1()
+	{
+		System.out.println("Main for demo running");
+		
+	}
+
+}
