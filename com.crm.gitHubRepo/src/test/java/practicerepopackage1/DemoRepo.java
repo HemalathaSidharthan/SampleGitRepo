e27232974371420252548c668d821cb9fc711e75
Admin <Admin@Admin-PC>
2022-03-24 12:19:05 +0530

    Owner commit 1

---
+++ com.crm.gitHubRepo/src/test/java/practicerepopackage1/DemoRepo.java
@@ -0,0 +1,12 @@
+package practicerepopackage1;
+
+import org.testng.annotations.Test;
+
+public class DemoRepo { 
+	@Test
+	public void main()
+	{
+		System.out.println("User 1 committed first time");
+	}
+
+}
